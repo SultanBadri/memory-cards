0c3e47d74de46f4dffafa7357f9c6a66d71eb0b3
Sultan Badri <46205282+SultanBadri@users.noreply.github.com>
2021-01-08 23:29:33 -0800

    added more cards

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import styled from "styled-components";
+
+const ModalDiv = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+`;
 
 export const Modal = () => {
+  //   return (
+  //     <ModalDiv>
+  //       <h1>hi</h1>
+  //     </ModalDiv>
+  //   );
   alert("You lost!");
 };
--- src/data/cardsData.tsx
+++ src/data/cardsData.tsx
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Card } from "../components/Card";
 import forceOfNature from "../images/force_of_nature.jpg";
+import capper from "../images/capper.png";
+import sandman from "../images/sandman.png";
 import directHit from "../images/direct_hit.jpg";
+import gunboats from "../images/gunboats.png";
+import escapePlan from "../images/escape_plan.png";
 import backburner from "../images/backburner.jpg";
+import flareGun from "../images/flare_gun.png";
+import powerjack from "../images/powerjack.jpg";
 import ironBomber from "../images/iron_bomber.png";
+import stickyJumper from "../images/sticky_jumper.png";
+import eyelander from "../images/eyelander.jpg";
+import tomislav from "../images/tomislav.png";
+import sandvich from "../images/sandvich.png";
+import glovesOfRunningUrgently from "../images/gloves_of_running_urgently.png";
+import rescueRanger from "../images/rescue_ranger.png";
+import shortCurcuit from "../images/short_circuit.png";
+import jag from "../images/jag.png";
+import crusaderCrossbow from "../images/crusader's_crossbow.png";
+import kritzkrieg from "../images/kritzkrieg.png";
+import ubersaw from "../images/ubersaw.png";
+import huntsman from "../images/huntsman.png";
+import razerback from "../images/razerback.png";
+import shashasha from "../images/shashasha.png";
+import ambassador from "../images/ambassador.png";
+import kunai from "../images/kunai.png";
+import deadRinger from "../images/dead_ringer.png";
 
 interface CardInterface {
   name: string;
@@ -35,6 +58,98 @@ export const CardContainer: React.FC<ScoreProps> = ({ score, setScore }) => {
     {
       name: "Iron Bomber",
       src: ironBomber,
+    },
+    {
+      name: "Tomislav",
+      src: tomislav,
+    },
+    {
+      name: "Kritzkrieg",
+      src: kritzkrieg,
+    },
+    {
+      name: "Rescue Ranger",
+      src: rescueRanger,
+    },
+    {
+      name: "Huntsman",
+      src: huntsman,
+    },
+    {
+      name: "Kunai",
+      src: kunai,
+    },
+    {
+      name: "Sandman",
+      src: sandman,
+    },
+    {
+      name: "Escape Plan",
+      src: escapePlan,
+    },
+    {
+      name: "Powerjack",
+      src: powerjack,
+    },
+    {
+      name: "Eyelander",
+      src: eyelander,
+    },
+    {
+      name: "Gloves of Running Urgently",
+      src: glovesOfRunningUrgently,
+    },
+    {
+      name: "Jag",
+      src: jag,
+    },
+    {
+      name: "Ubersaw",
+      src: ubersaw,
+    },
+    {
+      name: "Shashasha",
+      src: shashasha,
+    },
+    {
+      name: "Ambassador",
+      src: ambassador,
+    },
+    {
+      name: "Capper",
+      src: capper,
+    },
+    {
+      name: "Dead Ringer",
+      src: deadRinger,
+    },
+    {
+      name: "Razerback",
+      src: razerback,
+    },
+    {
+      name: "Crusader's Crossbow",
+      src: crusaderCrossbow,
+    },
+    {
+      name: "Short Curcuit",
+      src: shortCurcuit,
+    },
+    {
+      name: "Sandvich",
+      src: sandvich,
+    },
+    {
+      name: "Sticky Jumper",
+      src: stickyJumper,
+    },
+    {
+      name: "Flare Gun",
+      src: flareGun,
+    },
+    {
+      name: "Gunboats",
+      src: gunboats,
     },
   ];
 
